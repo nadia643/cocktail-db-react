dbd763fcbc2ff6199305f59a13cd74836f439c05
John Doe <nadia643@gmail.com>
2020-04-20 12:20:33 +0100

    search form done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,55 @@
 import React, {useEffect }from "react";
-import CocktailsList from "../components/CocktailList";
+import CocktailList from "../components/CocktailList";
 import SearchForm from "../components/SearchForm";
 
 export default function Home() {
   const [loading, setLoading] = React.useState(false);
-  const [searchTerm, setsearchTerm] = React.useState('a');
+  const [searchTerm, setSearchTerm] = React.useState("a");
   const [cocktails, setCocktails] = React.useState([]);
 
 
   // We set up the empty array and it means we only run this function when the componenent 
   // mounts, and not after each and every render
-
   useEffect(() => {
-    fetch(
-      `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`
-      )
-    .then(response => response.json())
-    .then(data => setCocktails(data.drinks));
+    setLoading(true);
+    async function getDrinks() {
+      try {
+        const response = await fetch(
+          `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`
+          );
+          const data = await response.json();
+          const { drinks } = data;
+          if (drinks) {
+            const newCocktails = drinks.map(item => {
+              const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass }
+              = item;
+              return { 
+                id: idDrink, 
+                name: strDrink, 
+                image: strDrinkThumb,
+                info: strAlcoholic,
+                glass: strGlass
+              }
+            });
+            setCocktails(newCocktails)
+          }
+          else {
+            setCocktails([]);
+          }
+        } catch (error) {     
+          console.log(error);
+        }
+        // after the catch, done with loading (false). Once the loading is done 
+      //   we are displaying the cocktails or that we can't find any that match
+        setLoading(false);
+      }
+      getDrinks();
   }, [searchTerm])
 
   return (
     <main>
-      <SearchForm setsearchTerm={setsearchTerm} />
-      <CocktailsList loading={loading} cocktails={cocktails} />
+      <SearchForm setSearchTerm={setSearchTerm} />
+      <CocktailList cocktails={cocktails} loading={loading} />
     </main>
   );
 }
